b388fa9a5da57213f4b40eadfb45ffe8f4cf4824
David Tomschitz <david@tomschitz.de>
2020-11-09 21:05:38 +0100

    added different animations for grip and point fingers

---
+++ Assets/Scripts/HandAnimator.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class HandAnimator : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public XRController controller;
+
+    private Animator animator;
+
+    private readonly List<Finger> gripFingers = new List<Finger>()
+    {
+        new Finger(FingerType.Middle),
+        new Finger(FingerType.Ring),
+        new Finger(FingerType.Pinky),
+    };
+
+    private readonly List<Finger> pointFingers = new List<Finger>()
+    {
+        new Finger(FingerType.Index),
+        new Finger(FingerType.Thumb),
+    };
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            SetFingerTargets(gripFingers, gripValue);
+        }
+
+        if (controller.inputDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            SetFingerTargets(pointFingers, triggerValue);
+        }
+
+        SmoothFinger(pointFingers);
+        SmoothFinger(gripFingers);
+
+        AnimateFingers(pointFingers);
+        AnimateFingers(gripFingers);
+    }
+
+    private void SetFingerTargets(List<Finger> fingers, float value)
+    {
+        foreach(Finger finger in fingers)
+        {
+            finger.target = value;
+        }
+    }
+
+    private void SmoothFinger(List<Finger> fingers)
+    {
+        foreach (Finger finger in fingers)
+        {
+            float time = speed * Time.unscaledDeltaTime;
+            finger.current = Mathf.MoveTowards(finger.current, finger.target, time);
+        }
+    }
+
+    private void AnimateFingers(List<Finger> fingers)
+    {
+        foreach (Finger finger in fingers)
+        {
+            animator.SetFloat(finger.type.ToString(), finger.current);
+        }
+    }
+}
