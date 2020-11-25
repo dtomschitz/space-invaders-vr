339629a52f9318f11b1891a648f5d2d1e6d4acc6
Duane <de030@hdm-stuttgart.de>
2020-11-25 11:00:05 +0100

    added AI chases player logic. Set-Up for attack range too.

---
+++ Assets/Scripts/AI/EnemyHuntsPlayer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyHuntsPlayer : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public LayerMask whatIsPlayer;
+
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+
+    public float attackRange;
+    public bool playerInAttackRange;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        //Check for attack range
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+
+        if(playerInAttackRange)
+        {
+            AttackPlayer();
+        }
+        ChasePlayer();
+    }
+
+    private void ChasePlayer()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    private void AttackPlayer()
+    {
+        //Make sure enemy doesn't move
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(player);
+
+        ///attackcode here
+
+        if (!alreadyAttacked)
+        {
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+    }
+
+}
