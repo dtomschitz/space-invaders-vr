e6e4d8697d7e818e8212d0178c66b3f3057fb0ad
Duane <de030@hdm-stuttgart.de>
2020-12-28 11:14:34 +0100

    added SceneFader to PauseMenu and MainMenu

---
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToLoad = "Main";
+
+    public SceneFader sceneFader;
+
+    public void Play()
+    {
+        sceneFader.FadeTo(levelToLoad);
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Exiting...");
+        Application.Quit();
+    }
+}
