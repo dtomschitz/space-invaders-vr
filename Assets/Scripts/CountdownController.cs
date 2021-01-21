09d9799baa25b15b55e291d234b0a034d93dd7bd
David Tomschitz <david@tomschitz.de>
2021-01-21 18:50:42 +0100

    further improvements for the ui

---
+++ Assets/Scripts/CountdownController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class CountdownController : MonoBehaviour
+{
+    public float time;
+    public TextMeshProUGUI countdownText;
+
+    private void Start()
+    {
+        StartCoroutine(Countdown());
+    }
+
+    IEnumerator Countdown()
+    {
+        while (time > 0)
+        {
+            countdownText.text = time.ToString();
+            yield return new WaitForSeconds(1f);
+            time--;
+        }
+
+        countdownText.text = "GO!";
+        GameState.instance.SetState(GameStateType.InGame);
+
+        yield return new WaitForSeconds(1f);
+        countdownText.gameObject.SetActive(false);
+    }
+}
