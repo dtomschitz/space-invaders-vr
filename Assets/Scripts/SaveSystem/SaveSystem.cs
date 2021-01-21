9cbb23a95a7ab034f19f934b29b938441b11eb06
Duane <de030@hdm-stuttgart.de>
2021-01-21 09:44:35 +0100

    added GameData und SaveSystem Scripts

---
+++ Assets/Scripts/SaveSystem/SaveSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public static class SaveSystem {
+
+    public static void SaveData (Player player, ForceField forceField, Enemy enemy)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + "/player.txt";
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        GameData gameData = new GameData(player, forceField, enemy);
+
+        formatter.Serialize(stream, gameData);
+        stream.Close();
+    }
+
+    public static GameData LoadGame()
+    {
+        string path = Application.persistentDataPath + "/player.txt";
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+
+            GameData gameData = formatter.Deserialize(stream) as GameData;
+            stream.Close();
+
+            return gameData;
+        } 
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+
+}
