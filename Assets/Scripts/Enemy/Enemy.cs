ef8276a52ff70e2499653fbfac694d5831a73244
Arz Sundar <sa070@hdm-stuttgart.de>
2021-01-29 18:13:10 +0100

    dodge left and right bug fixed

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.AI;
+using System.Collections;
 
 public enum EnemyState
 {
@@ -31,12 +32,17 @@ public class Enemy : Entity
     public int dodgeRange;
     public float dodgeStart;
     public float dodgeTime;
+    public GameObject[] waypoints;
+    int current = 0;
+    float WPradius = 1;
+
 
     public EnemyState State { get; protected set; }
 
     DodgeDirection dodgeDirection;
     NavMeshAgent agent;
     GameObject attackPoint;
+    public float speed;
 
     protected override void Start()
     {
@@ -52,6 +58,7 @@ public class Enemy : Entity
 
         timeBetweenAttacks = startTimeBtwAttacks;
         dodgeDirection = (DodgeDirection) Random.Range(0, System.Enum.GetValues(typeof(DodgeDirection)).Length);
+        
     }
 
     private void Update()
@@ -77,24 +84,27 @@ public class Enemy : Entity
 
         if (dodgeTime <= 0)
         {
-            if (dodgeDirection == DodgeDirection.RIGHT)
-            {
-                Dodge(dodgeRange);
-            }
-            else
-            {
-                Dodge(-dodgeRange);
-            }
-
+            
+          
+            dodgeDirection = dodgeDirection == DodgeDirection.LEFT ? DodgeDirection.RIGHT : DodgeDirection.LEFT;
             dodgeTime = dodgeStart;
         }
         else
         {
             dodgeTime -= Time.deltaTime;
-            Vector3 destination = agent.transform.position;
-            destination.z -= 1;
-            MoveToPosition(destination);
+            
         }
+
+        if (dodgeDirection == DodgeDirection.RIGHT)
+        {
+            Dodge(dodgeRange);
+        }
+        else
+        {
+            Dodge(-dodgeRange);
+        }
+
+        
 
     }
 
@@ -124,18 +134,15 @@ public class Enemy : Entity
     void Dodge(int range)
     {
         Vector3 destination = agent.transform.position;
-        destination.x += range;
+        destination.x = destination.x + range;
         MoveToPosition(destination);
-        dodgeDirection = dodgeDirection == DodgeDirection.LEFT ? DodgeDirection.RIGHT : DodgeDirection.LEFT;
+        
     }
 
     void MoveToPosition(Vector3 position)
     {
-        agent.SetDestination(position);
-        if (agent.transform.position == position)
-        {
-            agent.speed = 0;
-        }
+        transform.position = Vector3.MoveTowards(transform.position,position, Time.deltaTime * agent.speed);
+        
     }
 
     void ShowExplosion()
