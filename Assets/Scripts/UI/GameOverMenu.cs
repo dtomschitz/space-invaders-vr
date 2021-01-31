e3c3e2f278e391972883818d5d636f25f068dad1
David Tomschitz <david@tomschitz.de>
2021-01-31 14:05:05 +0100

    improved the menus and added integrated the statistics into the game over menu

--- Assets/Scripts/UI/GameOverMenu.cs
+++ Assets/Scripts/UI/GameOverMenu.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
+    public TextMeshProUGUI kills;
+    public TextMeshProUGUI time;
+
+    void OnEnable()
+    {
+        kills.SetText(Statistics.instance.Kills.ToString());
+        time.SetText(Statistics.instance.PlayedTime.ToString());
+    }
+
     public void Replay()
     {
         Scene scene = SceneManager.GetActiveScene();
