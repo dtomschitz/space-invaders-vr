60eff61101bf51f8a2b83650963a8bf498584805
David Tomschitz <david@tomschitz.de>
2021-01-31 16:17:41 +0100

    added a glowing hover effect to the menus

---
+++ Assets/Scripts/UI/ButtonHiglighter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class ButtonHiglighter : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Material defaultMaterial;
+    public Material highlightMaterial;
+
+    TextMeshProUGUI text;
+
+    void Start()
+    {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text.fontMaterial = highlightMaterial;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        text.fontMaterial = defaultMaterial;
+    }
+}
